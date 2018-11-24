b0d7ccd4c2810ed9cbf082f5d3debb1e68de3681
yzlee <1072432511@qq.com>
2018-11-24 18:06:19 +0800

    pom整理

--- admin-service/src/main/java/com/yzlee/goods/GoodsServiceApplication.java
+++ admin-service/src/main/java/com/yzlee/goods/GoodsServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableTransactionManagement
 public class GoodsServiceApplication {
 
 	public static void main(String[] args) {
